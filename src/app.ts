6d5ceac49c4b0dc14d67d5bf697ed987d4592002
mdshahjalal2 <muhommodshahjalal@gmail.com>
2023-05-17 14:50:20 +0600

    part 7 has done

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,24 @@
 // !n! !imp! toggle escape in vs code 
 
+// ! cojur => cors> json>urlEconded>route
+
 import express, { Application } from "express";
 const app: Application = express();
 
 // Application routes
 import userRoutes from "./app/modules/user/user.route";
-
+// ! cors 
 import cors from "cors";
 // using cors
 app.use(cors());
 
+// !json 
 // parse data
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
+// ! route 
 app.use("/api/v1/user", userRoutes);
 
 export default app;
