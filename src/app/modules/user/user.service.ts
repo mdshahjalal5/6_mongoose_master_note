fd35e69297eca768308b96e7730698a1d6cb8466
mdshahjalal2 <muhommodshahjalal@gmail.com>
2023-05-19 08:31:15 +0600

    part 7 has done

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -10,7 +10,7 @@
 // !qufis =>query> field filtering>sort
 // !imex=>  explicit and implicit $and > can't use impicit and when property is same 
 
-
+// !gap! huge gap mongodb nested query 
 /*
   collection.find({age:30 , age:{$gte:30}}) //! can't use age property is same // we can solve it by explicit $and operator 
  */
