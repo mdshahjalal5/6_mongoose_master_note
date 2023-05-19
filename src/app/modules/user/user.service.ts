fc6e7d614e5c3190e0e3b1ab67bd770a41305265
mdshahjalal2 <muhommodshahjalal@gmail.com>
2023-05-19 16:49:45 +0600

    part 7 has done

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -8,7 +8,7 @@
 
 // ! -----------------------// read 
 
-// !  mo quagso> mongodb operator> qup> query & projection operator> update operator> aggregration> so> aggregration pipeline stage> a
+// !  mo quagso> mongodb operator> qup> query & projection operator> update operator> aggregration> so> aggregration pipeline stage> aggregation pipeline operatore
 
 // !qufis =>query> field filtering>sort
 // !imex=>  explicit and implicit $and > can't use impicit and when property is same 
